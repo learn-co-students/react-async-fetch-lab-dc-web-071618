3d21c075667797145fbddeb869b967257048d064
john-raber <jrab224@gmail.com>
2018-09-20 08:22:46 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
-// create your App component here
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      astronauts: []
+    };
+  }
+
+  getAllAstronauts = () => {
+    fetch("http://api.open-notify.org/astros.json")
+      .then(r => r.json())
+      .then(json => {
+        this.setState({
+          astronauts: json.people
+        });
+      });
+  };
+
+  render() {
+    return this.state.astronauts.map(astro => {
+      return <div className="ui card">{astro.name}</div>;
+    });
+  }
+
+  componentDidMount() {
+    this.getAllAstronauts();
+  }
+}
+
+export default App;
